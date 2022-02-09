8e8d7bb60a1d3539a3ba2ff68b866c59d9f1b2a3
Ankush Pandit <ankush.pandit@gmail.com>
2022-02-08 18:10:39 -0600

    We were on a <br>

--- mycv/src/components/pages/credits/Credits.js
+++ mycv/src/components/pages/credits/Credits.js
@@ -13,7 +13,7 @@ class Credits extends Component {
 						<h6>
                         <i class="fas fa-info-circle"></i> <strong>  Credits</strong>
 						</h6>
-						This webpage is developed and maintained by Ankush Pandit and source can be found at <a href="https://github.com/iamankushpandit/full_stack_resume">https://github.com/iamankushpandit/full_stack_resume</a>.
+						This webpage is developed and maintained by Ankush Pandit and source can be found at <br></br><a href="https://github.com/iamankushpandit/full_stack_resume">https://github.com/iamankushpandit/full_stack_resume</a>.
 					</div>
 				</div>
 			</div>
